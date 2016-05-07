cc00ba551ae2bf8d8a1b7d49c058f48718643679
danilofsti <danilofs.ti@gmail.com>
2016-05-07 15:53:47 -0300

    correção

--- routes/estabelecimento.js
+++ routes/estabelecimento.js
@@ -13,13 +13,13 @@ exports.fazerCadastro = function() {
  * @example
  *   findAll('cardapio', {});
  *
- * @param   {JSON} query    O Filtro de dados para haver o retorno certo
- * @param   {} res   R
+ * @param   {JSON}  query   O Filtro de dados para haver o retorno certo
+ * @param   {}      res
  */
 function findEstabelecimento(query, res) {
 	database.find('estabelecimento', query, function (err, resources) {
 		res.writeHead(200, {'Content-Type': 'application/json'});
-		res.end(JSON.stringify(resources));
+		res.end(JSON.stringify(query));
 	});
 };
 
@@ -30,8 +30,8 @@ function findEstabelecimento(query, res) {
  * @example
  *   insert('cardapio', {'nome':'cardapio-hestia', 'tipo':'opcionais'});
  *
- * @param   {JSON} resource    Conjunto de dados JSON que irá ser inserido
- * @param   {} res
+ * @param   {JSON}  resource  Conjunto de dados JSON que irá ser inserido
+ * @param   {}      res
 */
 var insertEstabelecimento = function (resource, res) {
 	database.insert('estabelecimento', resource, function (err, resource) {
